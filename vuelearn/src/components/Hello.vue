3fd1978d0f12bc87570212e86700cef805079d17
random <283739569@qq.com>
2017-09-22 01:11:25 +0800

    first comment Cheapter<3> 1-4

---
+++ vuelearn/src/components/Hello.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2 v-bind:class="[classA,{'redfont':hasError}]"  v-bind:style="linkCss">Essential Links</h2>
+
+    <Testcom :weight="weight"></Testcom>
+
+    <button v-on:click="changeWeight">changeHattWeight</button>
+
+
+    <p v-if="isShowPartA">PartA</p>
+    <p v-else>没有数据啦</p>
+
+
+    <a v-show="!isShowPartA">PartB</a>
+
+  </div>
+</template>
+
+<script>
+
+  import Testcom from './Testcom.vue'
+  export default {
+  methods:{
+    changeWeight(){
+      this.weight='999999999'
+      this.linkCss.color='black'
+      this.isShowPartA=!this.isShowPartA;
+    }
+  },
+  name: 'hello',
+  data () {
+    return {
+      classA:'bluefont',
+      hasError:true,
+      isShowPartA:false,
+      classStr:{
+        redfont:true,
+        bluefont:false
+      },
+      msg: 'Welcome to Your Vue.js Addpp',
+      message:'keke',
+      weight:'',
+      linkCss:{
+        'color':'red',
+        'font-size':'20px'
+      }
+    }
+  },
+  components:{
+    Testcom
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a ,p{
+  color: #42b983;
+}
+</style>
---
+++ vuelearn/src/components/Testcom.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+
+    <div >this is template body</div>
+
+    <div v-for="item in list">{{'姓名:'+item.name+'   体重'+item.weight +'特别体重:'+weight}} </div>
+  <button v-on:click="addItem">addItem</button>
+
+    <a v-bind:href="baiduLink">go to Baidu now~</a>
+
+  </div>
+</template>
+
+
+<script>
+
+  import Vue from 'vue'
+  export default {
+    props:['weight'],
+    methods:{
+      addItem() {
+        Vue.set(this.list,1,{
+          name:'肥肥罗昊旻',
+          weight:'90'
+        })
+        console.log('hahahhaa')
+        this.list.push(this.fatHat);
+      }
+    },
+    data() {
+      return {
+        msg: 'hello vue',
+        baiduLink:'www.baidu.com',
+        list:[
+          {
+            name:'jiahao',
+            weight:'50'
+          },
+          {
+            name:'luohaomin',
+            weight:'60'
+          }
+        ],
+        fatHat:{
+          name:'feifeiHatt',
+          weight:'999'
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  body {
+    background-color: white;
+  }
+</style>
