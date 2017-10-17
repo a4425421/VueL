346463f71a68d44dca641779c1b7659d511ea0ef
Random <283739569@qq.com>
2017-10-17 18:07:03 +0800

    slidePicture

---
+++ vuelearn/src/components/DDSDF.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div>this is template body</div>
+    </div>
+</template>
+
+
+<script>
+
+    export default {
+        data() {
+            return {
+                msg: 'hello vue'
+            }
+        },
+
+
+    }
+</script>
+
+<style>
+    body {
+        background-color: #ffffff;
+    }
+</style>
